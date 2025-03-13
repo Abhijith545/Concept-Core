7f7b953cd66f5397d1e62f05ab207b5432ae6c73
Raparthi Abhijith <132045783+Abhijith545@users.noreply.github.com>
2025-03-13 23:58:10 +0530

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import './App.css';
 import { Search } from './components/search'
 import { Navbar } from './components/Navbar'
 
 function App() {
+  function Explore() {
+    return <>
+      <input placeholder='search for a course'></input>
+      <button>search</button>
+    </>
+  }
+  function MyCourses() {
+    return <>
+      <h1>All the courses</h1>
+    </>
+  }
+  function Profile() {
+    return <>
+      <h1>Profile</h1>
+    </>
+  }
   return (
     <>
+      <BrowserRouter>
       <Navbar />
-      <Search />
+      <Routes>
+        <Route path="/" index element={<>
+          <h1 style={{textAlign: 'center', marginTop: 5,}}>Welcome!</h1><br/>
+          <Search />
+        </>} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/mycourses" element={<MyCourses />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
