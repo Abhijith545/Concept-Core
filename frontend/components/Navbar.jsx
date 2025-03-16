f5e4aef5d24a1ee3eaf4fe43ae731b7d9a43f0d0
Raparthi Abhijith <132045783+Abhijith545@users.noreply.github.com>
2025-03-16 11:29:57 +0530

    Update Navbar.jsx

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -7,7 +7,11 @@ export function Navbar() {
                 <li><button><Link to="/">Home</Link></button></li>
                 <li><button><Link to="/explore">Explore</Link></button></li>
                 <li><button><Link to="/mycourses">My Courses</Link></button></li>
-                <li><button className="r8-btn"><Link to="/profile">Profile</Link></button></li>
+            </ul>
+            <ul className="nav-right">
+                <li><button><Link to="/profile">Profile</Link></button></li>
+                <li><button><Link to="/logout">Logout</Link></button></li>
+                <li><button><Link to="/signup">Signup/Login</Link></button></li>
             </ul>
         </div>
     </>
