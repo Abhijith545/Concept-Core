65b5189b295a8fc58ae14855834a691c95787521
Raparthi Abhijith <132045783+Abhijith545@users.noreply.github.com>
2025-03-26 11:03:03 +0530

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,49 +1,62 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
-import './App.css';
-import { Search } from './components/search'
-import { Navbar } from './components/Navbar'
-import { Title } from './components/Title';
-import { Signup } from './components/Signup';
-import { Login } from './components/Login';
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+import { Search } from "./components/Search";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Title } from "./components/Title/Title";
+import { Signup } from "./components/SignUp/Signup";
+import { Login } from "./components/Login/Login";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [isLogin, setLogin] = useState(false);
 
   function Explore() {
-    return <>
-      <Search placeholder="Search for a Course" />
-    </>
+    return <Search placeholder="Search for a Course" />;
   }
+
   function MyCourses() {
-    return <>
-      <h1>All the courses</h1>
-    </>
+    return <h1 style={{textAlign:"center", color:"white"}}>All the courses</h1>;
   }
+
   function Profile() {
-    return <>
-      <h1>Profile</h1>
-    </>
-  }
+    return <h1 style={{textAlign:"center", color:"white"}}>Profile</h1>;
+  } 
+
   return (
-    <>
-      <BrowserRouter>
-      <Title />
-      <Navbar />
-      <Routes>
-        <Route path="/" index element={<>
-          <h1 style={{textAlign: 'center', marginTop: 5,}}>Welcome!</h1><br/>
-          <Search placeholder="Search for a topic"/>
-        </>} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/mycourses" element={<MyCourses />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/signup" element={<Signup />}></Route>
-      </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      {isLogin ? (
+        <>
+          <Title />
+          <Navbar setLogin={setLogin} />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <h1 style={{ textAlign: "center", color: "white" }}>Welcome!</h1>
+                  <br />
+                  <Search placeholder="Search for a topic" />
+                </>
+              }
+            />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/mycourses" element={<MyCourses />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </>  
+      ) : (
+        <>
+          <Routes>
+            <Route path="/login" element={<Login setLogin={setLogin} />} />
+            <Route path="/signup" element={<Signup setLogin={setLogin} />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </>
+      )}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
